b6c978f2aaae788ac56f75fa85d745a597332b38
satvikjambagi <157267219+7vik2005@users.noreply.github.com>
2025-07-26 19:30:28 +0530

    [1976] [Time Beats: 11.97%] [Memory Beats: 55.18%] - LeetPush

---
+++ Number-of-Ways-to-Arrive-at-Destination.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int, int>> adj[n];
+        for (auto it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        using ll = long long;
+        priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
+        vector<ll> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+        int mod = 1e9 + 7;
+
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            auto [dis, node] = pq.top();
+            pq.pop();
+
+            for (auto [adjNode, wt] : adj[node]) {
+                if (dis + wt < dist[adjNode]) {
+                    dist[adjNode] = dis + wt;
+                    pq.push({dist[adjNode], adjNode});
+                    ways[adjNode] = ways[node];
+                } else if (dis + wt == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        return ways[n - 1];
+    }
+};
