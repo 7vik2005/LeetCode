ed88e6ab956c9e4047a83ea9345bfd51ba98e860
satvikjambagi <157267219+7vik2005@users.noreply.github.com>
2025-08-24 22:00:35 +0530

    [1493] [Time Beats: 7.51%] [Memory Beats: 59.08%] - LeetPush

---
+++ DCP-08-25/Longest-Subarray-of-1's-After-Deleting-One-Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left=0, right=0, ans=0, zero=0;
+        for(right=0;right<nums.size();right++)
+        {
+            if(nums[right]==0) zero++;
+            while(zero>1)
+            {
+                if(nums[left]==0) zero--;
+                left++;
+            }
+            ans=max(ans, right-left);
+        }
+        return ans;
+    }
+};
