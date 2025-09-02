2afed7377f189adc6070780eb2246c7faae60cad
satvikjambagi <157267219+7vik2005@users.noreply.github.com>
2025-09-02 17:59:06 +0530

    [3146] [Time Beats: 22.49%] [Memory Beats: 24.98%] - LeetPush

---
+++ Permutation-Difference-between-Two-Strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        unordered_map<char, int> mpp;
+        for(int i=0;i<s.length();i++)
+        {
+            mpp[s[i]]=i;
+        }
+        int ans=0;
+        for(int i=0;i<t.length();i++)
+        {
+            ans+=abs(i-mpp[t[i]]);
+        }
+        return ans;
+    }
+};
